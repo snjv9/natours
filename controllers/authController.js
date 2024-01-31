da89847bfbfcfa3af549112015ea80b63401036c
snjv9 <snjvchoudhary9@gmail.com>
2024-01-31 19:31:04 +0530

    Ready for Deployment

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,7 +16,8 @@ const createAndSendToken = async (user, statusCode, res) => {
     const cookieOptions = {
         expires: new Date(Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000),
         httpOnly: true,//can only be read
-        sameSite: "Lax",
+        sameSite: "none",
+        secure: true
 
     }
     if (process.env.NODE_ENV === 'production') {
